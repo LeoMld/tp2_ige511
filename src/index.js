ce975ee49d9c72b8be32139d9fe8d3abcd8323c8
Léo Mollard <leo.mollard@usherbrooke.ca>
2022-03-14 11:06:02 -0400

    Init project

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import _ from "lodash";
+import './style.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function component() {
     const element = document.createElement('div');
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('bg-gray');
 
     return element;
 }
